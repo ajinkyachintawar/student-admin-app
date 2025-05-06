9f8f61865494075376929f6cef1463dc0dc3e855
Ajinkya Chintawar <ajinkyachintawar12@gmail.com>
2025-05-06 22:21:59 +0530

    🚀 Ready for Netlify deployment with .env support

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://hqwappsdvwqhwwzkngta.supabase.co'; // replace with your URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhxd2FwcHNkdndxaHd3emtuZ3RhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1Mzk1NTcsImV4cCI6MjA2MjExNTU1N30.W-Un5PDTDvymcmN93NyNUuHPDs3cOvV_gozSq19dYVc'; // replace with your anon key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
